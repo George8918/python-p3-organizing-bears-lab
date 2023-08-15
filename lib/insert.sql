fb29f6cdf2075fe3319a11b624460a30f96e7b80
George8918 <GZheng8918@gmail.com>
2023-08-15 00:47:52 -0700

    complete

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears(name,age,sex,color,temperament,alive) VALUES('Mr. Chocolate',1,'M','black','annoy','true');
+INSERT INTO bears(name,age,sex,color,temperament,alive) VALUES('Rowdy',2,'M','black','annoy','true');
+INSERT INTO bears(name,age,sex,color,temperament,alive) VALUES('Tabitha',3,'M','black','annoy','true');
+INSERT INTO bears(name,age,sex,color,temperament,alive) VALUES('Sergeant Brown',4,'M','black','annoy','true');
+INSERT INTO bears(name,age,sex,color,temperament,alive) VALUES('Melissa',5,'M','black','annoy','false');
+INSERT INTO bears(name,age,sex,color,temperament,alive) VALUES('Grinch',6,'M','black','annoy','false');
+INSERT INTO bears(name,age,sex,color,temperament,alive) VALUES('Wendy',7,'M','black','annoy','true');
+INSERT INTO bears(name,age,sex,color,temperament,alive) VALUES(NULL,8,'M','black','annoy','true');
